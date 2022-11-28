392cf6c18cee7625fe6f95348e9d9781431dd322
Linkinlog <dahlton@linkd.dev>
2022-11-28 10:04:59 -0500

    Adds missing challenge

--- CodeWars/Go/movingZerosToTheEnd/movingZeros.go
+++ CodeWars/Go/movingZerosToTheEnd/movingZeros.go
@@ -1,16 +1,12 @@
 package movingZeros
 
-import "fmt"
-
 func MoveZeros(arr []int) []int {
-	for i, e := range arr {
-		fmt.Print(e)
-		fmt.Print(arr[:i+1])
-		fmt.Print(" ")
-		fmt.Print(arr[i+2:])
-		fmt.Println()
-		if e == 0 {
-			arr = append(arr[:i+1], arr[i+2:]...)
+	var array_len = len(arr)
+	var last_non_zero_index = 0
+	for i := 0; i < array_len; i++ {
+		if arr[i] != 0 {
+			arr[last_non_zero_index], arr[i] = arr[i], arr[last_non_zero_index]
+			last_non_zero_index++
 		}
 	}
 	return arr
